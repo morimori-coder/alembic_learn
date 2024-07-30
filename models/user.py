68d61821a099327f03f7ef9c7a49beb2dc56481f
ryo-seed <r.moriya@seed-it.jp>
2024-07-30 19:59:46 +0000

    不要なファイルを削除 and 移動により削除

--- models/user.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from db_setting.db_base import Base
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
