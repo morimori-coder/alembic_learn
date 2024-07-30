dd4fd4f59fc8a8dbf97b2ad136be8b02de4de3eb
ryo-seed <r.moriya@seed-it.jp>
2024-07-30 20:11:52 +0000

    DBへの接続情報を追記

--- app/db_setting/db_base.py
+++ app/db_setting/db_base.py
@@ -1,6 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.orm import sessionmaker
 
 # 統一されたMetaDataオブジェクトを作成
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
+
+# 接続情報を設定
+DATABASE_URL = "mssql+pyodbc://sa:saPassword1234@172.22.0.3:1433/alembic-learn?driver=ODBC+Driver+17+for+SQL+Server"
+
+# エンジンを作成
+engine = create_engine(DATABASE_URL)
+
+# セッションを作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
